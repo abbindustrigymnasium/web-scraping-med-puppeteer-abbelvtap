02d32d9f399f17adcfe02620491945ac0b542137
abbelvtap <92145452+abbelvtap@users.noreply.github.com>
2023-05-29 13:36:29 +0200

    commented dataHandler.py

--- dataHandler.py
+++ dataHandler.py
@@ -2,29 +2,37 @@ import json
 from collections import Counter
 import matplotlib.pyplot as plt
 
+
+# Öppna data.json och spara innehållet i variabeln data
 with open('data.json') as f: 
     data = json.loads(f.read())
 
+# skapar ett objekt där antalet av olika tecken sparas
 letters = {}
 
+# skriver vilka tecken jag vill räkna
 characters = 'abcdefghijklmnopqrstuvwxyz.,:;!?\'\"'
 
+# skapar en lista där tecken antalet för varje låt sparas
 count = []
 
+# räknar hur många av varje tecken det finns i varje låt och lägger till det i listan count
 for song in data:
     res = dict(Counter(song.lower()))
     count.append(res)
 
+# lägger ihop antalet tecken i olika låtar
 for char in characters:
     letters[char] = 0
 
+    # lägger till antalet an ett visst tecken från varje sång till totala räkningen
     for song in count:
         try:
             letters[char] += song[char]
         except:
-            print()
             continue
 
+# sorterar objektet med det totala antalet av olika tecken i storleksordning med den största först
 letters = dict(sorted(letters.items(), key=lambda x: x[1], reverse=True))
 
 # x-coordinates of left sides of bars 
